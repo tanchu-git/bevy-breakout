4f5886e6fdf54d9854a9dd5fd2e1568f344bad91
Tan Chu <tan.winh@gmail.com>
2023-11-23 00:49:01 +0100

    added walls and some func

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{math::vec3, prelude::*};
+use bevy::{
+    math::{vec2, vec3},
+    prelude::*,
+};
 
 // Paddle details
 const PADDLE_START_Y: f32 = 0.0;
@@ -13,24 +16,52 @@ const BALL_SIZE: Vec2 = Vec2::new(30.0, 30.0);
 const BALL_SPEED: f32 = 400.0;
 const BALL_INIT_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
 
+// Border wall
+const LEFT_WALL: f32 = -450.0;
+const RIGHT_WALL: f32 = 450.0;
+const BOTTOM_WALL: f32 = -300.0;
+const TOP_WALL: f32 = 300.0;
+
+const WALL_THICKNESS: f32 = 10.0;
+const WALL_BLOCK_WIDTH: f32 = RIGHT_WALL - LEFT_WALL;
+const WALL_BLOCK_HEIGHT: f32 = TOP_WALL - BOTTOM_WALL;
+const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, move_paddle)
+        .add_systems(FixedUpdate, (move_paddle, apply_velocity))
         .run();
 }
 
 #[derive(Component)]
 struct Paddle;
 
-fn setup(mut commands: Commands) {
-    // Camera
+#[derive(Component)]
+struct Ball;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Collider {
+    size: Vec2,
+}
+
+#[derive(Bundle)]
+struct WallBundle {
+    sprite_bundle: SpriteBundle,
+    collider: Collider,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Spawn camera
     commands.spawn(Camera2dBundle::default());
 
-    // Paddle
+    // Spawn paddle
     commands.spawn((
         SpriteBundle {
             transform: Transform {
@@ -46,6 +77,106 @@ fn setup(mut commands: Commands) {
         },
         Paddle,
     ));
+
+    // Load texture and spawn ball
+    let ball_texture = asset_server.load("textures/ball.png");
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: BALL_STARTING_POS,
+                ..default()
+            },
+            sprite: Sprite {
+                color: BALL_COLOR,
+                custom_size: Some(BALL_SIZE),
+                ..default()
+            },
+            texture: ball_texture,
+            ..default()
+        },
+        Ball,
+        Velocity(BALL_SPEED * BALL_INIT_DIRECTION),
+    ));
+
+    // Spawn left wall
+    let vertical_wall_size = vec2(WALL_THICKNESS, WALL_BLOCK_HEIGHT + WALL_THICKNESS);
+    let horizontal_wall_size = vec2(WALL_BLOCK_WIDTH + WALL_THICKNESS, WALL_THICKNESS);
+
+    commands.spawn(WallBundle {
+        sprite_bundle: SpriteBundle {
+            transform: Transform {
+                translation: vec3(LEFT_WALL, 0.0, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: WALL_COLOR,
+                custom_size: Some(vertical_wall_size),
+                ..default()
+            },
+            ..default()
+        },
+        collider: Collider {
+            size: vertical_wall_size,
+        },
+    });
+
+    // Spawn right wall
+    commands.spawn(WallBundle {
+        sprite_bundle: SpriteBundle {
+            transform: Transform {
+                translation: vec3(RIGHT_WALL, 0.0, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: WALL_COLOR,
+                custom_size: Some(vertical_wall_size),
+                ..default()
+            },
+            ..default()
+        },
+        collider: Collider {
+            size: vertical_wall_size,
+        },
+    });
+
+    // Spawn bottom wall
+    commands.spawn(WallBundle {
+        sprite_bundle: SpriteBundle {
+            transform: Transform {
+                translation: vec3(0.0, BOTTOM_WALL, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: WALL_COLOR,
+                custom_size: Some(horizontal_wall_size),
+                ..default()
+            },
+            ..default()
+        },
+        collider: Collider {
+            size: horizontal_wall_size,
+        },
+    });
+
+    // Spawn top wall
+    commands.spawn(WallBundle {
+        sprite_bundle: SpriteBundle {
+            transform: Transform {
+                translation: vec3(0.0, TOP_WALL, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: WALL_COLOR,
+                custom_size: Some(horizontal_wall_size),
+                ..default()
+            },
+            ..default()
+        },
+        collider: Collider {
+            size: horizontal_wall_size,
+        },
+    });
 }
 
 fn move_paddle(
@@ -68,4 +199,13 @@ fn move_paddle(
         paddle_transform.translation.x + direction * PADDLE_SPEED * time_step.delta_seconds();
 
     paddle_transform.translation.x = x_pos;
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_step: Res<Time>) {
+    let delta_time = time_step.delta_seconds();
+
+    for (mut transform, velocity) in &mut query {
+        transform.translation.x += velocity.x * delta_time;
+        transform.translation.y += velocity.y * delta_time;
+    }
 }
